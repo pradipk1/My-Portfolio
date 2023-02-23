fd3fdbdfcd6bb97c6124ec63d492427220260f1c
pradipk1 <pradipshukla12699@gmail.com>
2023-02-23 17:42:11 +0530

    something got updated

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,12 @@
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './pages';
 
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Routes>
-        <Route path='/' element={<Home />}></Route>
-      </Routes> */}
       <Home />
     </BrowserRouter>
   );
